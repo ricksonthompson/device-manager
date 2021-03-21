096e80834624fb7d99eae34975243386691b0100
Rickson Thompson <ricksonthompson.dev@gmail.com>
2021-03-21 14:35:51 -0400

    criada a migration de categories

---
+++ backend/src/database/migrations/1616351396102-CreateCategories.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateCategories1616351396102
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories');
+  }
+}
